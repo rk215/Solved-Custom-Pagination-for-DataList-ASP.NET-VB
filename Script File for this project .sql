c848747070ff6a652ae94b7cbb258ea57448ead7
Rk <r.kprajapati215@gmail.com>
2017-04-15 11:39:14 +0530

    Script file for creating database table
    Copy this file for creating database table for this project.

---
+++ Script File for this project .sql
@@ -0,0 +1,43 @@
+create table tb
+(
+num  int identity primary key,
+nam varchar(30),
+product varchar(30)
+)
+
+select  * from tb
+
+select top 5 num from tb order by num desc 
+
+insert into tb values('rk','test1')
+insert into tb values('rk','test2')
+insert into tb values('rk','test3')
+insert into tb values('rk','test4')
+insert into tb values('rk','test5')
+insert into tb values('rk','test6')
+insert into tb values('rk','test7')
+insert into tb values('rk','test8')
+insert into tb values('rk','test9')
+insert into tb values('rk','test10')
+insert into tb values('rk','test11')
+insert into tb values('rk','test12')
+insert into tb values('rk','test13')
+insert into tb values('rk','test14')
+insert into tb values('rk','test15')
+insert into tb values('rk','test16')
+insert into tb values('rk','test17')
+insert into tb values('rk','test18')
+insert into tb values('rk','test19')
+insert into tb values('rk','test20')
+insert into tb values('rk','test21')
+insert into tb values('rk','test22')
+insert into tb values('rk','test23')
+insert into tb values('rk','test24')
+insert into tb values('rk','test25')
+
+insert into tb values('rk','test26')
+insert into tb values('rk','test27')
+insert into tb values('rk','test28')
+insert into tb values('rk','test29')
+insert into tb values('rk','test30')
+insert into tb values('rk','test31')
